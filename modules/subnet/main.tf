17548b01df7c2a70a0a6d74eb1709cf453f194c3
Roman Izvozchikov <Roman_Izvozchikov@epam.com>
2022-03-21 19:01:57 +0000

    Modified modules/subnet/main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,9 +1,18 @@
 data "aws_availability_zones" "working" {}
 
+data "terraform_remote_state" "s3" {
+  backend = "s3"
+  config = {
+    bucket = "introtask1-terragrunt-nvizzz1"
+    key = "dev/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_subnet" "public" {
   count = length(var.subnets_public)
 
-  vpc_id            = var.vpc_id
+  vpc_id            = data.terraform_remote_state.s3.vpc_id
   cidr_block        = element(var.subnets_public, count.index)
   availability_zone = element(slice(data.aws_availability_zones.working.names, 0, var.azs), count.index % var.azs)
 
@@ -13,7 +22,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   count = length(var.subnets_private)
 
-  vpc_id            = var.vpc_id
+  vpc_id            = data.terraform_remote_state.s3.vpc_id
   cidr_block        = element(var.subnets_private, count.index)
   availability_zone = element(slice(data.aws_availability_zones.working.names, 0, var.azs), count.index % var.azs)
 
