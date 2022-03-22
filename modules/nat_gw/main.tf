ea5da86ee3a75fd17d8525b2d827f59e4b29f753
Roman Izvozchikov <Roman_Izvozchikov@epam.com>
2022-03-22 14:32:37 +0000

    NAT Gateway module was fixed

--- modules/nat_gw/main.tf
+++ modules/nat_gw/main.tf
@@ -1,3 +1,12 @@
+data "terraform_remote_state" "subnet" {
+  backend = "s3"
+  config = {
+    bucket = "introtask1-terragrunt-nvizzz1"
+    key = "dev/subnet/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 resource "aws_eip" "nat-gw" {
   count = length(var.subnets_public)
 
@@ -11,7 +20,7 @@ resource "aws_nat_gateway" "cloudx" {
   count = length(var.subnets_public)
 
   allocation_id = aws_eip.nat-gw["${count.index}"].id
-  subnet_id     = var.subnets_public[count.index]
+  subnet_id     = data.terraform_remote_state.subnet.outputs.subnets_public[count.index]
 
   tags = { Name = "nat-gw-${count.index + 1}" }
 }
