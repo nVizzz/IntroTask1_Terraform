9818c3b5d4766b0107fc1ad389cac2388f733a81
Roman Izvozchikov <Roman_Izvozchikov@epam.com>
2022-03-22 14:02:13 +0000

    IGW module was added

---
+++ modules/igw/main.tf
@@ -0,0 +1,12 @@
+data "terraform_remote_state" "s3" {
+  backend = "s3"
+  config = {
+    bucket = "introtask1-terragrunt-nvizzz1"
+    key = "dev/vpc/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+resource "aws_internet_gateway" "cloudx" {
+  vpc_id = data.terraform_remote_state.s3.outputs.vpc_id
+}
---
+++ modules/igw/outputs.tf
@@ -0,0 +1,4 @@
+output "igw_id" {
+  description = "ID of the IGW"
+  value       = aws_internet_gateway.cloudx.id
+}
