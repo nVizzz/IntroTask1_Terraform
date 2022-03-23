6e4e6121f7643b15a3e8697958a7b06252635135
Roman Izvozchikov <Roman_Izvozchikov@epam.com>
2022-03-23 18:35:20 +0000

    launch_template module was fixed

--- modules/launch_template/main.tf
+++ modules/launch_template/main.tf
@@ -40,6 +40,7 @@ resource "aws_launch_template" "cloudx" {
   network_interfaces {
     associate_public_ip_address = var.associate_public_ip_address
     security_groups             = [data.terraform_remote_state.sg.outputs.sg_private]
+    network_card_index          = 0
   }
 
   tag_specifications {
